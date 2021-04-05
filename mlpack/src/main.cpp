9a6868ea917202c663fa8efdf541ca2aecfb7811
Lucas Maisonnave <lucas.maisonnave@etu.emse.fr>
2021-04-05 17:59:13 +0200

    rapidcsv

--- mlpack/src/main.cpp
+++ mlpack/src/main.cpp
@@ -1,26 +1,16 @@
 #include <mlpack/core.hpp>
+#include <stdio.h>
+#include <string>
 #include <mlpack/methods/neighbor_search/neighbor_search.hpp>
+#include "../include/rapidcsv.h"
 using namespace mlpack;
 using namespace mlpack::neighbor; // NeighborSearch and NearestNeighborSort
 using namespace mlpack::metric; // ManhattanDistance
+using namespace rapidcsv;
+using namespace std;
 int main()
 {
-  // Load the data from data.csv (hard-coded).  Use IO for simple command-line
-  // parameter handling.
-  arma::mat data;
-  data::Load("data.csv", data, true);
-  // Use templates to specify that we want a NeighborSearch object which uses
-  // the Manhattan distance.
-  NeighborSearch<NearestNeighborSort, ManhattanDistance> nn(data);
-  // Create the object we will store the nearest neighbors in.
-  arma::Mat<size_t> neighbors;
-  arma::mat distances; // We need to store the distance too.
-  // Compute the neighbors.
-  nn.Search(1, neighbors, distances);
-  // Write each neighbor and distance using Log.
-  for (size_t i = 0; i < neighbors.n_elem; ++i)
-  {
-    std::cout << "Nearest neighbor of point " << i << " is point "
-        << neighbors[i] << " and the distance is " << distances[i] << ".\n";
-  }
+  Document doc("../data/test.csv");
+  vector<string> moves = doc.GetColumn<string>("Moves");
+  cout << moves.size() << endl;
 }
