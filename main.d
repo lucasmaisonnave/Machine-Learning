66865f7b130b49312a91e3a131f8cd35530cb077
lucasmaisonnave <56760454+lucasmaisonnave@users.noreply.github.com>
2021-05-22 15:46:40 +0200

    Delete main.d

--- main.d
+++
@@ -1,5 +0,0 @@
-obj/main.o: src/main.cpp src/../include/Chess.h src/../include/DataCSV.h \
- src/../include/rapidcsv.h src/../include/ChessGame.h \
- src/../include/olcPixelGameEngine.h src/../include/DataCSV.h \
- src/../include/TFUtils.h src/../include/TFModel.h \
- src/../include/../include/DataCSV.h
